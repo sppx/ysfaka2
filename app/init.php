3c1e69fe54ab3b0c60bdf072d4392d49cc91091f
sppx <37325838+sppx@users.noreply.github.com>
2019-03-03 00:48:24 +0800

    Update init.php

--- app/init.php
+++ app/init.php
@@ -7,10 +7,10 @@ define('YS_ROOT',dirname(__FILE__));
 ob_start();header('Content-Type:text/html;charset=utf8');
 date_default_timezone_set('Asia/Shanghai');
 //自动加载
-require '/vendor/autoload.php';
-require_once 'https://fk.ppxwo.net/app/common.php';
-require_once 'https://fk.ppxwo.net/app/Config.php';
-require_once 'https://fk.ppxwo.net/app/ysapp.php';
+require '/app/htdocs/vendor/autoload.php';
+require_once '/app/htdocs/app/common.php';
+require_once '/app/htdocs/app/Config.php';
+require_once '/app/htdocs/app/ysapp.php';
 
 session_start();
 ?>
