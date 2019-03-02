e5bf051c18cf044971a0e94db1b8dbd8e5545c08
sppx <37325838+sppx@users.noreply.github.com>
2019-03-02 23:49:23 +0800

    Update init.php

--- app/init.php
+++ app/init.php
@@ -7,7 +7,7 @@ define('YS_ROOT',dirname(__FILE__));
 ob_start();header('Content-Type:text/html;charset=utf8');
 date_default_timezone_set('Asia/Shanghai');
 //自动加载
-require '/home/vcap/app/htdocs/app/vendor/autoload.php';
+require YS_ROOT.'../vendor/autoload.php';
 require_once YS_ROOT.'/common.php';
 require_once YS_ROOT.'/Config.php';
 require_once YS_ROOT.'/ysapp.php';
